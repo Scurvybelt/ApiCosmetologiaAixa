1122440ad1fbfb929f31fb02f8dcd8a403e6ec97
Scurvybelt <90409656+Scurvybelt@users.noreply.github.com>
2024-11-06 22:22:16 -0600

    fotosServicios

--- servicesModel.php
+++ servicesModel.php
@@ -133,8 +133,8 @@ class servicesModel{
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            // $mail->Username   = 'eduardoavilat2002@gmail.com';                     //SMTP username
-            // $mail->Password   = 'fyohvokyieibtuwm';                         //SMTP password
+            $mail->Username   = 'eduardoavilat2002@gmail.com';                     //SMTP username
+            $mail->Password   = 'fyohvokyieibtuwm';                         //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // ENCRYPTION_SMTPS 464 Enable implicit TLS encryption
             $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
